039798ae3f22041350427c6e935cf7287eea24b9
barsu5489 <ebarsulai@gmail.com>
2022-12-31 00:03:13 +0300

    recreated using chstGPT

--- movie.js
+++ movie.js
@@ -1,109 +1,55 @@
 /*
  * firstMovie - function to fetch and render first movie
- * mMovieData - func
+ * MovieData - func
  * renderData - function to render data to DOM   
 */
 
-function firstMovie()
-{
-   
+function firstMovie(movieData) {
+    // Replace the content of movie.innerHTML with the content of the clicked movie
+    const movie = document.querySelector('.movie');
+    movie.innerHTML = `
+      <div class="img">
+        <img src="${movieData.poster}" alt="poster" srcset="">
+      </div>
+      <div class="movie-details">
+        <h1>${movieData.title}</h1>
+        <ul id="details">
+          <li>Run Time: ${movieData.runtime} minutes</li>
+          <li>Show Time: ${movieData.showtime}</li>
+          <li>Available Tickets: ${movieData.capacity - movieData.tickets_sold}</li>
+        </ul>
+      </div>
+    `;
+  }
+  
+  function movieData() {
     fetch('https://my-json-server.typicode.com/Barsu5489/Flatdango/db')
-    .then(res => res.json())
-    .then(data => 
-        {   
-            console.log(data)
-            const movie = document.querySelector('.movie');
-            //Rendering first movie
-            movie.innerHTML = 
-            `
-            <div class="img">
-                <img src="${data.films[0].poster}" alt="poster" srcset="">
-            </div>
-            <div class="movie-details">
-            <h1>${data.films[0].title}</h1>
-              <ul id="details">
-                <li>Run Time: ${data.films[0].runtime} minutes</li>
-                <li>Show Time: ${data.films[0].showtime}</li>
-                <li>Available Tickets: ${data.films[0].capacity - data.films[0].tickets_sold}</li>
-              </ul>
-            </div>
-            `
-        })
-}
-function movieData()
-{
-    
-    fetch('https://my-json-server.typicode.com/Barsu5489/Flatdango/db')
-    .then(res =>res.json())
-    .then(data => 
-        {
-            renderData(data)
-        })    
-}
-let render = "";
-function renderData(data)
-{
-  //  const section = document.querySelector('.section');
-    for(let i =1; i< data.films.length; i++)
-        {   
-            // Container to all elements
-            const section = document.querySelector('.section');
-            //Creating image div container 
-            const imgDiv = document.createElement('div')
-            imgDiv.classList.add('img')
-            //creating image tag
-            const img = document.createElement('img')
-            img.src = data.films[i].poster
-            //creating div to contain movie details
-            const movieDetails = document.createElement('div')
-            movieDetails.classList.add('movie-details')
-            //creating header tag
-            const movieTitle = document.createElement('h1')
-            movieTitle.classList.add('heading')
-            movieTitle.innerText = data.films[i].title
-            // Appending elements into the container div
-            section.appendChild(imgDiv)
-            section.appendChild(movieDetails)
-            imgDiv.appendChild(img)
-            imgDiv.appendChild(movieTitle)
-            // Creating list elements and button 
-            const l1 = document.createElement('li')
-            const li2 = document.createElement('li')
-            const li3 = document.createElement('li')
-            const btn = document.createElement('button')
-            btn.innerText = 'Buy Ticket'
-            btn.classList.add('list4')
-            l1.innerText = `Run Time: ${data.films[i].runtime}`
-            li2.innerText = `Show Time: ${data.films[i].showtime}`
-            li3.innerText = data.films[i].capacity - data.films[i].tickets_sold
-            //Appending li and button
-            section.appendChild(l1)
-            section.appendChild(li2)
-            section.appendChild(li3)
-            section.appendChild(btn)
-
-                // Implementing button functionality
-               const btnn =  document.querySelector('.list4').addEventListener('click', (e)=>
-                {
-                    if(li3.textContent > 0)
-                    {
-                        li3.style.display = 'block'
-                        li3.textContent --
-                    }
-                    else if(li3.textContent <= 0)
-                    {
-                        btn.style.display = 'none'
-                        li3.textContent = 'sold Out'
-                    }        
-
-                })
-        
-        }
-}
-
-document.addEventListener('DOMContentLoaded', ()=>
-{
-    firstMovie()
-    movieData()
-    
-})
+      .then((res) => res.json())
+      .then((data) => {
+        renderData(data);
+      });
+  }
+  
+  function renderData(data) {
+    for (let i = 1; i < data.films.length; i++) {
+      // creating header tag
+      const movieTitle = document.createElement('h1');
+      movieTitle.innerText = data.films[i].title;
+  
+      // Appending movie title to the DOM
+      const section = document.querySelector('.section');
+      section.appendChild(movieTitle);
+  
+      // Add a click event listener to the movie title element
+      movieTitle.addEventListener('click', (event) => {
+        // Call the firstMovie function and pass it the data for the clicked movie
+        firstMovie(data.films[i]);
+      });
+    }
+  }
+  
+  document.addEventListener('DOMContentLoaded', () => {
+    movieData();
+    firstMovie();
+  });
+  
